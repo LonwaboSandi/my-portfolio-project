f04628b1060e5711328bf8333d22b76d12ed459c
LonwaboSandi <80621585+LonwaboSandi@users.noreply.github.com>
2021-06-23 10:21:59 +0200

    commit

---
+++ src/components/Portfolio.js
@@ -0,0 +1,141 @@
+import React from "react"; 
+import pic1 from "../images/pic1.png"; 
+import pic2 from "../images/pic2.jpg"; 
+import pic3 from "../images/pic2.png"; 
+import pic4 from "../images/pic4.jpg"; 
+import pic5 from "../images/pic5.jpg"; 
+import pic6 from "../images/pic6.jfif"; 
+import pic7 from "../images/pic7.jpg"; 
+
+const Portfolio =()=>{
+    return (
+        <div className="container-port">
+            <div class="title">
+                <h2>Portfolio</h2>
+                
+                </div>
+            <div className="box">
+                <div className="imgBox">
+                    <img  src={pic1} alt="pic1.."/>
+                </div>
+                <div className="details">
+                    <div className="content">
+                        <h2>What is Lorem</h2>
+                        <p>What is Lorem Ipsum Lorem Ipsum is 
+                            simply dummy text of the printing and typesetting 
+                            industry Lorem Ipsum has been the industry's standard dum
+                        </p>
+                    </div>
+                   
+                </div>
+            </div>
+            <div className="box">
+            <div className="imgBox">
+                    <img  src={pic2} alt="pic2.."/>
+                </div>
+                <div className="details">
+                    <div className="content">
+                        <h2>What is Lorem</h2>
+                        <p>What is Lorem Ipsum Lorem Ipsum is 
+                            simply dummy text of the printing and typesetting 
+                            industry Lorem Ipsum has been the industry's standard dum
+                        </p>
+                    </div>
+                   
+                </div>
+            </div>
+            <div className="box">
+            <div className="imgBox">
+                    <img  src={pic3} alt="pic3.."/>
+                </div>
+                <div className="details">
+                    <div className="content">
+                        <h2>What is Lorem</h2>
+                        <p>What is Lorem Ipsum Lorem Ipsum is 
+                            simply dummy text of the printing and typesetting 
+                            industry Lorem Ipsum has been the industry's standard dum
+                        </p>
+                    </div>
+                   
+                </div>
+            </div>
+            <div className="box">
+            <div className="imgBox">
+                    <img  src={pic4} alt="pic4.."/>
+                </div>
+                <div className="details">
+                    <div className="content">
+                        <h2>What is Lorem</h2>
+                        <p>What is Lorem Ipsum Lorem Ipsum is 
+                            simply dummy text of the printing and typesetting 
+                            industry Lorem Ipsum has been the industry's standard dum
+                        </p>
+                    </div>
+                   
+                </div>
+            </div>
+            <div className="box">
+            <div className="imgBox">
+                    <img  src={pic5} alt="pic5.."/>
+                </div>
+                <div className="details">
+                    <div className="content">
+                        <h2>What is Lorem</h2>
+                        <p>What is Lorem Ipsum Lorem Ipsum is 
+                            simply dummy text of the printing and typesetting 
+                            industry Lorem Ipsum has been the industry's standard dum
+                        </p>
+                    </div>
+                   
+                </div>
+            </div>
+            <div className="box">
+            <div className="imgBox">
+                    <img  src={pic6} alt="pic6.."/>
+                </div>
+                <div className="details">
+                    <div className="content">
+                        <h2>What is Lorem</h2>
+                        <p>What is Lorem Ipsum Lorem Ipsum is 
+                            simply dummy text of the printing and typesetting 
+                            industry Lorem Ipsum has been the industry's standard dum
+                        </p>
+                    </div>
+                   
+                </div>
+            </div>
+            <div className="box">
+            <div className="imgBox">
+                    <img  src={pic7} alt="pic7.."/>
+                </div>
+                <div className="details">
+                    <div className="content">
+                        <h2>What is Lorem</h2>
+                        <p>What is Lorem Ipsum Lorem Ipsum is 
+                            simply dummy text of the printing and typesetting 
+                            industry Lorem Ipsum has been the industry's standard dum
+                        </p>
+                    </div>
+                   
+                </div>
+            </div>
+            <div className="box">
+            <div className="imgBox">
+                    <img  src={pic1} alt="pic1.."/>
+                </div>
+                <div className="details">
+                    <div className="content">
+                        <h2>What is Lorem</h2>
+                        <p>What is Lorem Ipsum Lorem Ipsum is 
+                            simply dummy text of the printing and typesetting 
+                            industry Lorem Ipsum has been the industry's standard dum
+                        </p>
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Portfolio; 
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,17 +1,9 @@
 import React from "react"; 
 
 const Services =()=>{
-    return (
-        <div className='tc'>
-            <div className="Infomative" >
-                <p>Infomative website</p>
-            </div>
-            <div className="service">
-                <p>Service based websit</p>
-            </div>
-            <div className="ecommerce">
-                <p>ecomerce website</p>
-            </div>
+    return(
+        <div>
+            
         </div>
     )
 }
